8cf5d66ef81aaaef6772547d1c5f66dce6ac69e9
Ning <1700805832@qq.com>
2026-01-21 11:05:48 +0800

    update

--- packages/sui/src/index.ts
+++ packages/sui/src/index.ts
@@ -1,4 +1,8 @@
-export * from './chain';
+/**
+ * 导出入口（barrel）
+ * - 暴露链配置、Provider 组合与钱包相关能力
+ */
+export * from '../../assets/src/chains/sui';
 export * from './sui-provider';
 export * from './wallets/built-in';
 export * from './wallets/factory';
